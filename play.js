768e3ad840d5d81cb3f5cf725b45a34cfe61eae6
Adrian Kiva <akiva@mta.ca>
2021-12-01 17:01:21 -0500

    add stdin and exit condition

--- play.js
+++ play.js
@@ -1,7 +1,23 @@
 const { connect } = require('./client')
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 // establishes a connection with the game server
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 
 
 console.log("Connecting ...");
